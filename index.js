40e006b273c5ab6e93d9316b4159ef2b58bd92cb
lyan- <ryonakamuraryo@gmail.com>
2017-01-23 15:19:38 +0900

    move method

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
-var addLimitationMiddleware = require('./lib/add-limitation-middleware.js');
-module.exports = addLimitationMiddleware;
+var qs = require('qs');
+var bytes = require('bytes');
+
+/**
+ * set array limitation for loopback application as middleware
+ *
+ * @param {Object} [options]
+ * @return {Function} middleware
+ * @public
+ */
+
+module.exports = function(options) {
+  var opts = options || {};
+  var limit = bytes.parse(opts.limit);
+
+  if(limit == null) {
+    limit = 100;
+  }
+
+  return function addLimitationMiddleware(req, res, next) {
+    var app = req.app;
+    app.set('query parser', function(value, option) {
+      return qs.parse(value, {arrayLimit: limit});
+    });
+    next();
+  };
+};
